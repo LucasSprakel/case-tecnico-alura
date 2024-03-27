8b4b84983fda20495d2e381d09fb53570a7b6348
LucasSprakel <lucassprakel@gmail.com>
2024-03-27 20:30:34 -0300

    modify: Removendo import desnecessário

--- src/main/java/com/lucassprakel/alura/controllers/UsersController.java
+++ src/main/java/com/lucassprakel/alura/controllers/UsersController.java
@@ -1,7 +1,6 @@
 package com.lucassprakel.alura.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.MergedAnnotations.Search;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
