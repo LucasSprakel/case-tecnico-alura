22a3643626c9f6d6e7eb3fef36d41b23d30b5d60
LucasSprakel <lucassprakel@gmail.com>
2024-03-27 00:21:35 -0300

    feat: Adicionando Repository Registration

---
+++ src/main/java/com/lucassprakel/alura/repositories/RegistrationRepository.java
@@ -0,0 +1,11 @@
+package com.lucassprakel.alura.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucassprakel.alura.models.Registration;
+
+
+
+public interface RegistrationRepository extends JpaRepository<Registration, Long>{
+    boolean existsByUsernameAndCourseCode(String username, String courseCode);
+}
